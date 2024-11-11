27c15479d3fa9200d50d7aa8e0c29b206f0e7166
Akshat <uidev@encriss.com>
2024-11-11 10:55:16 +0530

    First commit

---
+++ hello/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="app-container">
+      <h1 className="hello-world">Hello, World!</h1>
+    </div>
+    </div>
+  );
+}
+
+export default App;
